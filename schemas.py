0c6c34239190b9b04c6ccae518c8e34ec7aeae7d
SajidriX <sajidrix@gmail.com>
2025-04-21 17:23:56 +0300

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+class User(BaseModel):
+    username: str
+    balance: float = 0
+    password: str
+
+class Fish(BaseModel):
+    name: Annotated[str, Field(min_length=3, max_length=28)]
+    price: Annotated[float, Field(ge=0,le=1000000)]
+    cathced: Annotated[str, Field(min_length=3,max_length=50)]
+
+class UserGet(BaseModel):
+    username: str
+    balance: float
+
+class UserCreate(BaseModel):
+    username: Annotated[str, Field(min_length=3, max_length=20)]
+    password: Annotated[str, Field(min_length=4, max_length=35)]
